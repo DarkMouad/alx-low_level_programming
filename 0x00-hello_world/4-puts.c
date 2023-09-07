75a26d1a1c54c5c9c663efbbec96732994286be4
DarkMouad <mouadhafidi41@gmail.com>
2023-09-07 11:21:16 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints with the put function
  *
- * Return: Always 0 (Succsess)
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-       	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
