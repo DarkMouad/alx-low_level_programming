3c2ced87ed7c542085edfde94f357249c68d43ec
DarkMouad <mouadhafidi41@gmail.com>
2023-09-14 11:09:35 -0700

    finished

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,17 +10,17 @@
 void print_diagonal(int n)
 {
 	int postn, space;
-
-	if (n <= 0)
-		_putchar('\n');
-	else
+    
+    if (n <= 0)
+	    _putchar('\n');
+    else
+    {
+	for (postn = 1; postn <= n; postn++)
 	{
-		for (postn = 1; postn <= n; postn++)
-		{
-			for (space = 1; space <= postn; space++)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
-		}
+		for (space = 1; space < postn; space++) // Change space <= postn to space < postn
+		    _putchar(' ');
+	    _putchar('\\'); // Change _putchar(92) to _putchar('\\')
+	    _putchar('\n');
 	}
+    }
 }
